b58e61c44e7fd9722b24cb7d849682dc9e46bba1
bilter1001 <bilter1001@163.com>
2021-03-21 21:45:47 +0800

    1、增加当前棋子的红圈显示 2、增加悔棋功能 3、增加重新开始功能 4、增加获取所有UI style的功能代码

--- Assets/Scripts/chess.cs
+++ Assets/Scripts/chess.cs
@@ -7,6 +7,7 @@
 
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class chess : MonoBehaviour {
 
@@ -29,20 +30,24 @@ public class chess : MonoBehaviour {
     float minGridDis;  //网格宽和高中较小的一个
     Vector2[,] chessPos; //存储棋盘上所有可以落子的位置
     int[,] chessState; //存储棋盘位置上的落子状态
+    Vector2 curCheesPos;//当前落子位置
     enum turn {black, white } ;
     turn chessTurn; //落子顺序
     public Texture2D white; //白棋子
     public Texture2D black; //黑棋子
+    public Texture2D ring; //圆环
     public Texture2D blackWin; //白子获胜提示图
     public Texture2D whiteWin; //黑子获胜提示图
     int winner = 0; //获胜方，1为黑子，-1为白子
     bool isPlaying = true; //是否处于对弈状态
-	void Start () {
+    Stack<Vector2> stackSteps = new Stack<Vector2>();
+    GUIStyle gUIStyle;
+    void Start () {
         chessPos = new Vector2[15, 15];
         chessState =new int[15,15];
         chessTurn = turn.black;
-
-	}
+        curCheesPos = new Vector2(-1,-1);;
+    }
 	
 	void Update () {
 
@@ -76,6 +81,9 @@ public class chess : MonoBehaviour {
                     {
                         //根据下棋顺序确定落子颜色
                         chessState[i, j] = chessTurn == turn.black ? 1 : -1;
+                        curCheesPos = new Vector2(i, j);
+                        //将每一步落子，放入到栈里面
+                        stackSteps.Push(curCheesPos);
                         //落子成功，更换下棋顺序
                         chessTurn = chessTurn == turn.black ? turn.white : turn.black;                        
                     }
@@ -99,18 +107,64 @@ public class chess : MonoBehaviour {
         //按下空格重新开始游戏
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            for (int i = 0; i < 15; i++)
-            {
-                for (int j = 0; j < 15; j++)
-                {
-                    chessState[i, j] = 0;
-                }
-            }
-            isPlaying = true;
-            chessTurn = turn.black;
-            winner = 0;
+            ReStart();
         }     
 	}
+
+    //重新开始游戏
+    void ReStart()
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            for (int j = 0; j < 15; j++)
+            {
+                chessState[i, j] = 0;
+            }
+        }
+        isPlaying = true;
+        chessTurn = turn.black;
+        winner = 0;
+        curCheesPos = new Vector2(-1, -1);
+        stackSteps.Clear();
+    }
+
+    //悔棋
+    void Undo()
+    {
+        //判断当前栈中是否存在以走步数,只有存在已走步数才能悔棋
+        if (stackSteps.Count > 0)
+        {
+            //弹出最后一步,并将其位置重置为没有棋子的状态
+            var lastStep = stackSteps.Pop();
+            int x = (int)lastStep.x;
+            int y = (int)lastStep.y;
+            chessState[x,y] = 0;
+            if (chessTurn == turn.black)
+            {
+                chessTurn = turn.white;
+            }
+            else
+            {
+                chessTurn = turn.black;
+            }
+
+            //并重新设置上一步为当前的棋子
+            if (stackSteps.Count == 0)
+            {
+                curCheesPos.x = -1;
+                curCheesPos.y = -1;
+            }
+            else
+            {
+                var prevStep = stackSteps.Peek();
+                curCheesPos.x = prevStep.x;
+                curCheesPos.y = prevStep.y;
+            }
+
+        }
+
+    }
+
     //计算平面距离函数
     float Dis(Vector3 mPos, Vector2 gridPos)
     {
@@ -118,9 +172,24 @@ public class chess : MonoBehaviour {
     }
 
     void OnGUI()
-    { 
+    {
+
+        GUIStyle buttonMid = GUI.skin.button;
+        gUIStyle = buttonMid;
+        gUIStyle.alignment = TextAnchor.MiddleCenter; //我们想让它居中对齐
+        gUIStyle.fontSize = 20;
+        //绘制当前落子的底图圆环
+        if (curCheesPos.x != -1 || curCheesPos.y != -1)
+        {
+            int curX = (int)curCheesPos.x;
+            int curY = (int)curCheesPos.y;
+            float heigt = gridHeight + gridHeight / 6;
+            float width = gridWidth + gridWidth / 6;
+            GUI.DrawTexture(new Rect(chessPos[curX, curY].x - width / 2, Screen.height - chessPos[curX, curY].y - heigt / 2, width, heigt), ring,ScaleMode.ScaleToFit, true, 0, Color.red, 0, 0);
+        }
+
         //绘制棋子
-        for(int i=0;i<15;i++)
+        for (int i=0;i<15;i++)
         {
             for (int j = 0; j < 15; j++)
             {
@@ -134,13 +203,32 @@ public class chess : MonoBehaviour {
                 }               
             }
         }
+
         //根据获胜状态，弹出相应的胜利图片
         if (winner ==  1)
         GUI.DrawTexture(new Rect(Screen.width * 0.25f, Screen.height * 0.25f, Screen.width * 0.5f, Screen.height * 0.25f), blackWin);
         if (winner == -1)
         GUI.DrawTexture(new Rect(Screen.width * 0.25f, Screen.height * 0.25f, Screen.width * 0.5f, Screen.height * 0.25f), whiteWin);
+        if (winner != 0)
+        {
 
+            if (GUI.Button(new Rect(Screen.width * 0.25f, Screen.height * 0.5f, Screen.width * 0.5f, Screen.height * 0.25f), "重开开始", gUIStyle))
+            {
+                ReStart();
+            }            
+        }
+
+        if (GUI.Button(new Rect(Screen.width * 0.03f, Screen.height * 0.04f, Screen.width * 0.08f, Screen.height * 0.08f), "悔棋", gUIStyle))
+        {
+            Undo();
+        }
+
+        if (GUI.Button(new Rect(Screen.width * 0.03f, Screen.height * 0.86f, Screen.width * 0.09f, Screen.height * 0.08f), "重新开始", gUIStyle))
+        {
+            ReStart();
+        }
     }
+
     //检测是够获胜的函数，不含黑棋禁手检测
     int result()
     {
